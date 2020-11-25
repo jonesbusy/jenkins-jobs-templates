b130d9b3cd9eed8f5c1f01ada1ea8f246b183a6b
Valentin Delaye <jonesbusy@gmail.com>
2020-11-25 20:55:17 +0100

    Add quarkus build

--- jobs.groovy
+++ jobs.groovy
@@ -1,1 +1,18 @@
-
+pipelineJob('quarkus-graalvm') {
+	displayName('quarkus-graalvm')
+	description('quarkus-graalvm build')
+	definition {
+		cpsScm {
+			scriptPath('Jenkinsfile')
+			scm {
+				git {
+					remote {
+						name('origin')
+						branch('master')
+						url('https://github.com/jonesbusy/quarkus-graalvm.git')
+					}
+				}
+			}
+		}
+	}
+}
